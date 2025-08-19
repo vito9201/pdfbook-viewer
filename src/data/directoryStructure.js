29ed7c0d5c2486d1ff1c0365d0d13c8771cbd1b3
vito9201 <vito920131@outlook.com>
2025-08-19 16:49:09 +0800

    Add directoryStructure.js data file

---
+++ src/data/directoryStructure.js
@@ -0,0 +1,54 @@
+// 静态目录结构数据
+// 此文件由directoryUtils.js生成
+// 请在PDFbook文件夹结构更改后重新生成
+
+export const directoryStructure = {
+  "name": "PDFbook",
+  "type": "directory",
+  "children": [
+    {
+      "name": "初中",
+      "type": "directory",
+      "children": [
+        {
+          "name": "初中（五•四学制）",
+          "type": "directory",
+          "children": [
+            {
+              "name": "地理",
+              "type": "directory",
+              "children": [
+                {
+                  "name": "鲁教版-山东教育出版社",
+                  "type": "directory",
+                  "children": [
+                    {
+                      "name": "六年级",
+                      "type": "directory",
+                      "children": [
+                        {
+                          "name": "初中地理-六年级上册-鲁教版（五四制）.pdf",
+                          "type": "file",
+                          "size": "20.5 MB",
+                          "modified": "2025-08-19 08:45:58"
+                        },
+                        {
+                          "name": "初中地理-六年级下册-鲁教版（五四制）.pdf",
+                          "type": "file",
+                          "size": "18.3 MB",
+                          "modified": "2025-08-19 08:45:58"
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
+
+export default directoryStructure;
